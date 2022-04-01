1f0de3a9c82d2219d9ba695cd30d0769460096c1
Valeria Vitvytska <leranmu@gmail.com>
2022-04-01 14:18:12 +0300

    next commit

---
+++ 2.js
@@ -0,0 +1,46 @@
+"use strict";
+
+
+let userName = prompt('Enter your name here!');
+
+let userAge = +prompt('Enter your age here');
+
+
+ if( userName === null || userName === ''|| isNaN(userAge) || userAge === 0 ){
+
+	 userName = prompt('Enter your name here to continue!',userName);
+
+     userAge = +prompt('Enter your age here',userAge);
+
+ }
+
+
+
+if( userAge < 18){
+
+	alert('You are not allowed to visit this website');
+
+} else if(userAge > 18 && userAge <= 22){
+
+	let confirmation = confirm('Are you sure you want to continue');
+
+	if (true){ 
+
+		alert('Welcome,' + userName);
+
+	}
+
+	else{
+
+    alert('You are not allowed to visit this website');
+
+	}
+}
+
+else{
+
+	alert('Welcome,' + userName);
+}
+	
+
+
